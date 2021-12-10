7b108c78ea7c559cc0a3de00660ece2bb720ffe2
Evan Chang <evanc577@gmail.com>
2021-12-09 22:56:07 -0500

    Day 9 refactor low_points

--- src/solutions/day_09.rs
+++ src/solutions/day_09.rs
@@ -44,7 +44,7 @@ fn part_02(input: &Array2<usize>) {
 }
 
 fn low_points(input: &Array2<usize>) -> Vec<(usize, usize)> {
-    let low_points: Vec<_> = input
+    input
         .windows([3, 3])
         .into_iter()
         .enumerate()
@@ -53,15 +53,13 @@ fn low_points(input: &Array2<usize>) -> Vec<(usize, usize)> {
                 .min(window[[1, 0]])
                 .min(window[[2, 1]])
                 .min(window[[1, 2]]);
-            let center = window[[1, 1]];
-            if center < min {
+            if window[[1, 1]] < min {
                 Some((i / (input.ncols() - 2) + 1, i % (input.ncols() - 2) + 1))
             } else {
                 None
             }
         })
-        .collect();
-    low_points
+        .collect()
 }
 
 fn basin_size(input: &Array2<usize>, point: (usize, usize)) -> usize {
