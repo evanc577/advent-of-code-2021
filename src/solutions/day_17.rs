3683e9e14ef32ba643d1467cfa9cb4ce31cfe5c2
Evan Chang <evanc577@gmail.com>
2021-12-17 02:10:21 -0500

    Day 17 part 2

--- src/solutions/day_17.rs
+++ src/solutions/day_17.rs
@@ -1,3 +1,5 @@
+use std::cmp::Ordering;
+
 use regex::Regex;
 
 use crate::prelude::*;
@@ -23,6 +25,9 @@ impl Day for Day17 {
             })
             .collect::<Result<_, _>>()?;
         if let [x1, x2, y1, y2] = values[..] {
+            if x1 > x2 || y1 > y2 {
+                return Err(AOCError::ParseError);
+            }
             Ok(Self {
                 target_x: (x1, x2),
                 target_y: (y1, y2),
@@ -37,16 +42,62 @@ impl Day for Day17 {
     }
 
     fn part_2(&self) -> Answer {
-        Answer::None
+        Answer::Integer(num_trajectories(self.target_x, self.target_y))
     }
 }
 
 fn max_y(target_y: (isize, isize)) -> usize {
+    // Assume that target is below y-axis and if the final y value is on target, then it is always
+    // possible to reach to target
     if target_y.0 < 0 && target_y.1 < 0 {
         let n = (target_y.0 + 1).abs();
         return (n * (n + 1) / 2) as usize;
     }
     unimplemented!();
+}
+
+fn num_trajectories(target_x: (isize, isize), target_y: (isize, isize)) -> usize {
+    // Assume that target is below y axis
+    if target_y.0 < 0 && target_y.1 < 0 {
+        let mut count = 0;
+        for y_velocity in (target_y.0)..=(-target_y.0) {
+            for x_velocity in target_x.0.min(0)..=target_x.1.max(0) {
+                let mut x = 0;
+                let mut y = 0;
+                let mut dx = x_velocity;
+                let mut dy = y_velocity;
+
+                // Simulate
+                while y >= target_y.0 {
+                    // Update positions / velocities
+                    x += dx;
+                    y += dy;
+                    match dx.cmp(&0) {
+                        Ordering::Greater => dx -= 1,
+                        Ordering::Less => dx += 1,
+                        _ => (),
+                    }
+                    dy -= 1;
+
+                    // Check if hit
+                    if contains(target_x, target_y, (x, y)) {
+                        count += 1;
+                        break;
+                    }
+                }
+            }
+        }
+
+        return count;
+    }
+    unimplemented!()
+}
+
+fn contains(target_x: (isize, isize), target_y: (isize, isize), coords: (isize, isize)) -> bool {
+    coords.0 >= target_x.0
+        && coords.0 <= target_x.1
+        && coords.1 >= target_y.0
+        && coords.1 <= target_y.1
 }
 
 #[cfg(test)]
